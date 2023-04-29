8b0170db733cf8e6c0046f773b1562c89953219e
nomnomnonono <murumuru1204@gmail.com>
2023-04-29 18:39:46 +0900

    format

--- app.py
+++ app.py
@@ -10,10 +10,13 @@ with gr.Blocks() as demo:
             with gr.Column(scale=1):
                 text_input = gr.Textbox(value="太鼓", label="SE Title")
                 audio_input = gr.Audio(source="upload")
-                ratio = gr.Slider(minimum=0, maximum=1, value=1, label="Weight Parameter. 1 means 'use only text'. 0 means 'use only audio'.")
-                topk = gr.Dropdown(
-                    [5, 10, 20, 30, 40, 50], value="20", label="Top K"
+                ratio = gr.Slider(
+                    minimum=0,
+                    maximum=1,
+                    value=1,
+                    label="1 means 'use only text'. 0 means 'use only audio'.",
                 )
+                topk = gr.Dropdown([5, 10, 20, 30, 40, 50], value="20", label="Top K")
                 button = gr.Button("Search")
             with gr.Column(scale=2):
                 output = gr.Dataframe()
@@ -22,7 +25,12 @@ with gr.Blocks() as demo:
             with gr.Column(scale=1):
                 mic_text_input = gr.Textbox(value="太鼓", label="SE Title")
                 mic_audio_input = gr.Audio(source="microphone")
-                mic_ratio = gr.Slider(minimum=0, maximum=1, value=1, label="Weight Parameter. 1 means 'use only text'. 0 means 'use only audio'.")
+                mic_ratio = gr.Slider(
+                    minimum=0,
+                    maximum=1,
+                    value=1,
+                    label="1 means 'use only text'. 0 means 'use only audio'.",
+                )
                 mic_topk = gr.Dropdown(
                     [5, 10, 20, 30, 40, 50], value="20", label="Top K"
                 )
@@ -34,7 +42,9 @@ with gr.Blocks() as demo:
         search.search, inputs=[text_input, audio_input, ratio, topk], outputs=output
     )
     mic_button.click(
-        search.search, inputs=[mic_text_input, mic_audio_input, mic_ratio, mic_topk], outputs=mic_output
+        search.search,
+        inputs=[mic_text_input, mic_audio_input, mic_ratio, mic_topk],
+        outputs=mic_output,
     )
 
 demo.launch()
